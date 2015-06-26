8048d6d98b12c70584c535dd69c559a36695885e
Vid Jercic <vid.jercic@gmail.com>
2015-06-26 12:51:08 +0200

    'first1'

--- db.php
+++ db.php
@@ -3,11 +3,7 @@
 $password = '';
 $server = 'localhost';
 $db_name = 'bestof'; 
-/*
-$username = 'u248967365_vido';
-$password = 'vido66';
-$server = 'mysql.hostinger.co.uk';
-$db_name = 'u248967365_vido';*/
+
 
 $link = mysqli_connect($server, $username, $password, $db_name);
 mysqli_query($link, "SET NAMES 'utf8'");
